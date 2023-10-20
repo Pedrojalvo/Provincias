23bbe076c501c0f90d453827173f2050c2872322
Pedro <pedrojalvoperezlozao@gmail.com>
2023-10-20 12:59:17 +0200

    codigo

---
+++ src/dominio/Localidad.java
@@ -0,0 +1,25 @@
+package Ejercicio21.dominio;
+
+public class Localidad {
+    private String nombre;
+    private int numeroDeHabitantes;
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    
+    public int getNumeroDeHabitantes() {
+        return numeroDeHabitantes;
+    }
+    public void setNumeroDeHabitantes(int numeroDeHabitantes) {
+        this.numeroDeHabitantes = numeroDeHabitantes;
+    }
+
+    
+    public String toString() {
+        return "Localidad [nombre=" + nombre + ", numeroDeHabitantes=" + numeroDeHabitantes + "]";
+    }
+}
---
+++ src/dominio/Provincia.java
@@ -0,0 +1,27 @@
+package dominio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Provincia {
+    private List<Municipio> municipios = new ArrayList<>();
+
+     
+    public void agregarMunicipio(Municipio municipio) {
+        municipios.add(municipio);
+    }
+
+    
+    public int contarHabitantes() {
+        int totalHabitantes = 0;
+        for (Municipio municipio : municipios) {
+            totalHabitantes += municipio.contarHabitantes();
+        }
+        return totalHabitantes;
+    }
+
+    
+    public String toString() {
+        return "Provincia [municipios=" + municipios + "]";
+    }
+}
